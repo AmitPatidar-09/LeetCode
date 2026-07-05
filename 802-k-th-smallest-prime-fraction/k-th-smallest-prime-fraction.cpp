7be8e3113de6156e70964be3d3560209246fa371
Amit Patidar <amitpatidar056@gmail.com>
2026-07-05 18:00:51 +0530

    Time: 944 ms (15.25%) | Memory: 178.8 MB (13.38%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    typedef pair<double,pair<int,int>> pp;
+    typedef vector<double> V;
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<pp>pq;
         int n = arr.size();
-        for(int i = 0; i < n-1; i++)
-        {
-            for(int j = i+1; j < n; j++)
-            {
-                pq.push({arr[i]*1.0 / arr[j], {arr[i],arr[j]}});
 
-                if(pq.size() > k)
-                pq.pop();
-            }
+        priority_queue<V, vector<V>, greater<V>>
+            pq; // min-heap {fraction, i, j}
+        for (int i = 0; i < n - 1; i++) {
+            pq.push({1.0 * arr[i] / arr[n - 1], (double)i, (double)n - 1});
         }
 
-        auto x = pq.top().second;
-        return {x.first,x.second};
+        int smallest = 1;
+        while (smallest < k) {
+            V vec = pq.top();
+            pq.pop();
+            smallest++;
+            int i = vec[1];
+            int j = vec[2] - 1;
+            pq.push({1.0 * arr[i] / arr[j], (double)i, (double)j});
+        }
+        V vec = pq.top();
+        pq.pop();
+        int i = vec[1];
+        int j = vec[2];
+        return {arr[i], arr[j]};
     }
 };
