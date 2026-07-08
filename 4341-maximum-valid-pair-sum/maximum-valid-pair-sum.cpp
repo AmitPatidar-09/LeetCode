851c85923fcb3736e8a004ef693ca324bb5c3d78
Amit Patidar <amitpatidar056@gmail.com>
2026-07-08 23:07:03 +0530

    Time: 7 ms (27.34%) | Memory: 215.3 MB (37.15%) - LeetSync

--- 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.cpp
+++ 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxValidPairSum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>pref(n,0);
-        pref[n-1] = nums[n-1];
+        vector<int>suff(n,0);
+        suff[n-1] = nums[n-1];
         for(int i = n-2; i >=0; i--)
         {
-            pref[i] = max(pref[i+1], nums[i]);
+            suff[i] = max(suff[i+1], nums[i]);
         }
         int ans = 0;
         for(int j = 0; j < n-k; j++)
         {
-            ans = max(ans,nums[j] + pref[j+k]);
+            ans = max(ans,nums[j] + suff[j+k]);
         }
         return ans;
     }
