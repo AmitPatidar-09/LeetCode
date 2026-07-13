ed0a30f0ef9a9816e092e71e3bcc4f921b72dd97
Amit Patidar <amitpatidar056@gmail.com>
2026-07-13 12:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.96%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        string temp = "123456789";
-        vector<int> nums;
-        for (int i = 0; i < temp.size(); i++) {
-            string x = "";
-            for (int j = i; j < temp.size(); j++) {
-                x += temp[j];
-                int num = stoi(x);
-                nums.push_back(num);
+        string s = "123456789";
+        vector<int> ans;
+
+        int lenLow = to_string(low).size();
+        int lenHigh = to_string(high).size();
+
+        for (int len = lenLow; len <= lenHigh; len++) {
+            for (int i = 0; i + len <= 9; i++) {
+                int num = stoi(s.substr(i, len));
+
+                if (num >= low && num <= high)
+                    ans.push_back(num);
             }
         }
-        vector<int>ans;
-        sort(nums.begin(), nums.end());
-        for(int i = 0 ; i < nums.size(); i++)
-        {
-            if(nums[i] >= low && nums[i] <= high)
-            ans.push_back(nums[i]);
-        }
+
         return ans;
     }
 };
