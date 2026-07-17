f9d07d43760a121b1c19237bd6f6b9666032aa05
Amit Patidar <amitpatidar056@gmail.com>
2026-07-17 14:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (33.95%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -26,18 +26,21 @@ public:
         vector<int> ans;
         TreeNode* node = root;
         stack<TreeNode*> st;
-        while (true) {
-
-            if (node) {
+        while(true)
+        {
+            if(node)
+            {
                 st.push(node);
                 node = node->left;
-            } else {
-                if (st.empty())
-                    break;
+            }
+            else
+            {
+                if(st.empty()) break;
                 node = st.top();
                 st.pop();
                 ans.push_back(node->val);
                 node = node->right;
+                // st.push(node);
             }
         }
         return ans;
