435d48a0460c4513b7dca075ba3f631c5677c150
Amit Patidar <amitpatidar056@gmail.com>
2026-07-20 18:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (91.78%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -3,29 +3,25 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int row = grid.size();
         int col = grid[0].size();
-
+        int n = row * col;
+        k = k % n;
         
-        vector<int>nums;
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < col; j++)
-            {
-                nums.push_back(grid[i][j]);
+        // Create a result grid of the same size
+        vector<vector<int>> result(row, vector<int>(col));
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // Find the new 1D index
+                int new_flat_idx = (i * col + j + k) % n;
+                
+                // Convert back to 2D coordinates
+                int new_r = new_flat_idx / col;
+                int new_c = new_flat_idx % col;
+                
+                result[new_r][new_c] = grid[i][j];
             }
         }
-        k = k % nums.size() ;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin() + k);
-        reverse(nums.begin()+k, nums.end());
-        int idx = 0;
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < col; j++)
-            {
-                grid[i][j] = nums[idx++];
-            }
-            
-        }
-        return grid;
+        
+        return result;
     }
 };
